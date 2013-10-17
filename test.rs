5b78deb0f47de8d6e76f709c578c2ba3483fdf3e
Ian Daniher <it.daniher@gmail.com>
2013-10-16 20:08:11 -0400

    rewrite most of test.rs to use higher level abstraction API

--- test.rs
+++ test.rs
@@ -4,7 +4,6 @@ extern mod OpenCL;
 use OpenCL::CL::*;
 use OpenCL::CL::ll::*;
 use std::ptr;
-use std::io;
 use std::sys;
 use std::libc;
 use std::vec;
@@ -13,176 +12,135 @@ use std::cast;
 #[fixed_stack_segment]
 fn main()
 {
-  unsafe
-  {
-    let ker = 
-        ~"__kernel void vector_add(__global const long *A,
-                                   __global const long *B,
-                                   __global       long *C) {
-             int i = get_global_id(0);
-             C[i] = A[i] + B[i];
-         }";
+	unsafe
+	{
+		let ker =
+				~"__kernel void vector_add(__global const long *A, __global const long *B, __global long *C) {
+					int i = get_global_id(0);
+					C[i] = A[i] + B[i];
+				 }";
 
-    let sz    = 8;
-    let vec_a = ~[0, 1, 2, -3, 4, 5, 6, 7];
-    let vec_b = ~[-7, -6, 5, -4, 0, -1, 2, 3];
+		let sz: uint = 8;
+		let vec_a = ~[0, 1, 2, -3, 4, 5, 6, 7];
+		let vec_b = ~[-7, -6, 5, -4, 0, -1, 2, 3];
 
-    let p_id:   cl_platform_id = ptr::null();
-    let device: cl_device_id   = ptr::null();
-    let np:     cl_uint        = 0;
-    let nd:     cl_uint        = 0;
-    let mut r:  cl_int;    
+		let mut r: cl_int = 1;
 
-    // Get the platform and device information
-    clGetPlatformIDs(1,
-                     ptr::to_unsafe_ptr(&p_id),
-                     ptr::to_unsafe_ptr(&np));
+		let platforms = OpenCL::hl::get_platforms();
 
-    r = clGetDeviceIDs(p_id,
-                       CL_DEVICE_TYPE_ALL,
-                       1,
-                       ptr::to_unsafe_ptr(&device),
-                       ptr::to_unsafe_ptr(&nd));
+		let device = OpenCL::hl::get_devices(platforms[0], CL_DEVICE_TYPE_ALL)[0];
+		println(fmt!("%?", device.name()));
 
-    if r != CL_SUCCESS as cl_int
-    { io::println(fmt!("Can't get device ID. [%?]", r)); }
+		let ctx = OpenCL::hl::create_context(device);
 
-    // Create OpenCL context and command queue
-    let ctx = clCreateContext(ptr::null(),
-                              nd,
-                              ptr::to_unsafe_ptr(&device),
-                              cast::transmute(ptr::null::<&fn ()>()),
-                              ptr::null(),
-                              ptr::to_unsafe_ptr(&r));
+		let cque = OpenCL::hl::create_command_queue(&ctx, device);
+		//let cque = comque.cqueue;
+		let A = OpenCL::hl::create_buffer(&ctx, sz*8, CL_MEM_READ_ONLY);
+		let B = OpenCL::hl::create_buffer(&ctx, sz*8, CL_MEM_READ_ONLY);
+		let C = OpenCL::hl::create_buffer(&ctx, sz*8, CL_MEM_WRITE_ONLY);
 
-    let cque = clCreateCommandQueue(ctx, device, 0, ptr::to_unsafe_ptr(&r));
+		// Copy lists into memory buffers
+		clEnqueueWriteBuffer(cque.cqueue,
+			A.buffer,
+			CL_TRUE,
+			0,
+			(sz * sys::size_of::<int>()) as libc::size_t,
+			vec::raw::to_ptr(vec_a) as *libc::c_void,
+			0,
+			ptr::null(),
+			ptr::null());
 
-    // Create memory buffers
-    let A = clCreateBuffer(ctx,
-                           CL_MEM_READ_ONLY,
-                           (sz * sys::size_of::<int>()) as libc::size_t,
-                           ptr::null(),
-                           ptr::to_unsafe_ptr(&r));
+		clEnqueueWriteBuffer(cque.cqueue,
+			B.buffer,
+			CL_TRUE,
+			0,
+			(sz * sys::size_of::<int>()) as libc::size_t,
+			vec::raw::to_ptr(vec_b) as *libc::c_void,
+			0,
+			ptr::null(),
+			ptr::null());
 
-    let B = clCreateBuffer(ctx,
-                           CL_MEM_READ_ONLY,
-                           (sz * sys::size_of::<int>()) as libc::size_t,
-                           ptr::null(),
-                           ptr::to_unsafe_ptr(&r));
+		// Create a program from the kernel and build it
+		do ker.as_imm_buf |bytes, len|
+		{
+			let prog = clCreateProgramWithSource(ctx.ctx,
+						1,
+						ptr::to_unsafe_ptr(&(bytes as *libc::c_char)),
+						ptr::to_unsafe_ptr(&(len as libc::size_t)),
+						ptr::to_unsafe_ptr(&r));
 
-    let C = clCreateBuffer(ctx,
-                           CL_MEM_WRITE_ONLY,
-                           (sz * sys::size_of::<int>()) as libc::size_t,
-                           ptr::null(),
-                           ptr::to_unsafe_ptr(&r));
+			r = clBuildProgram(prog,
+				1,
+				ptr::to_unsafe_ptr(&device.id),
+				ptr::null(),
+				cast::transmute(ptr::null::<&fn ()>()),
+				ptr::null());
 
-    // Copy lists into memory buffers
-    clEnqueueWriteBuffer(cque,
-                         A,
-                         CL_TRUE,
-                         0,
-                         (sz * sys::size_of::<int>()) as libc::size_t,
-                         vec::raw::to_ptr(vec_a) as *libc::c_void,
-                         0,
-                         ptr::null(),
-                         ptr::null());
+			if r != CL_SUCCESS as cl_int
+			{ println(fmt!("Unable to build program [%?].", r)); }
 
-    clEnqueueWriteBuffer(cque,
-                         B,
-                         CL_TRUE,
-                         0,
-                         (sz * sys::size_of::<int>()) as libc::size_t,
-                         vec::raw::to_ptr(vec_b) as *libc::c_void,
-                         0,
-                         ptr::null(),
-                         ptr::null());
+			// Create the OpenCL kernel
+			do "vector_add".as_imm_buf() |bytes, _|
+			{
+				let kernel = clCreateKernel(prog, bytes as *libc::c_char, ptr::to_unsafe_ptr(&r));
+				if r != CL_SUCCESS as cl_int
+				{ println(fmt!("Unable to create kernel [%?].", r)); }
 
-    // Create a program from the kernel and build it
-    do ker.as_imm_buf |bytes, len|
-    {
-      let prog = clCreateProgramWithSource(ctx,
-                                           1,
-                                           ptr::to_unsafe_ptr(&(bytes as *libc::c_char)),
-                                           ptr::to_unsafe_ptr(&(len as libc::size_t)),
-                                           ptr::to_unsafe_ptr(&r));
+				// Set the arguments of the kernel
+				clSetKernelArg(kernel,
+					0,
+					sys::size_of::<cl_mem>() as libc::size_t,
+					ptr::to_unsafe_ptr(&A.buffer)	 as *libc::c_void);
 
-      r = clBuildProgram(prog,
-                         nd,
-                         ptr::to_unsafe_ptr(&device),
-                         ptr::null(),
-                         cast::transmute(ptr::null::<&fn ()>()),
-                         ptr::null());
+				clSetKernelArg(kernel,
+					1,
+					sys::size_of::<cl_mem>() as libc::size_t,
+					ptr::to_unsafe_ptr(&B.buffer)	 as *libc::c_void);
 
-      if r != CL_SUCCESS as cl_int
-      { io::println(fmt!("Unable to build program [%?].", r)); }
+				clSetKernelArg(kernel,
+					2,
+					sys::size_of::<cl_mem>() as libc::size_t,
+					ptr::to_unsafe_ptr(&C.buffer)	 as *libc::c_void);
 
-      // Create the OpenCL kernel
-      do "vector_add".as_imm_buf() |bytes, _|
-      {
-        let kernel = clCreateKernel(prog, bytes as *libc::c_char, ptr::to_unsafe_ptr(&r));
-        if r != CL_SUCCESS as cl_int
-        { io::println(fmt!("Unable to create kernel [%?].", r)); }
+				let global_item_size: libc::size_t = (sz * sys::size_of::<int>()) as libc::size_t;
+				let local_item_size:	libc::size_t = 64;
 
-        // Set the arguments of the kernel
-        clSetKernelArg(kernel,
-                       0,
-                       sys::size_of::<cl_mem>() as libc::size_t,
-                       ptr::to_unsafe_ptr(&A)   as *libc::c_void);
+				// Execute the OpenCL kernel on the list
+				clEnqueueNDRangeKernel(cque.cqueue,
+					kernel,
+					1,
+					ptr::null(),
+					ptr::to_unsafe_ptr(&global_item_size),
+					ptr::to_unsafe_ptr(&local_item_size),
+					0,
+					ptr::null(),
+					ptr::null());
 
-        clSetKernelArg(kernel,
-                       1,
-                       sys::size_of::<cl_mem>() as libc::size_t,
-                       ptr::to_unsafe_ptr(&B)   as *libc::c_void);
+				// Now let's read back the new list from the device
+				let buf = libc::malloc((sz * sys::size_of::<int>()) as libc::size_t);
 
-        clSetKernelArg(kernel,
-                       2,
-                       sys::size_of::<cl_mem>() as libc::size_t,
-                       ptr::to_unsafe_ptr(&C)   as *libc::c_void);
+				clEnqueueReadBuffer(cque.cqueue,
+					C.buffer,
+					CL_TRUE,
+					0,
+					(sz * sys::size_of::<int>()) as libc::size_t,
+					buf,
+					0,
+					ptr::null(),
+					ptr::null());
 
-        let global_item_size: libc::size_t = (sz * sys::size_of::<int>()) as libc::size_t;
-        let local_item_size:  libc::size_t = 64;
+				let vec_c = vec::from_buf(buf as *int, sz);
 
-        // Execute the OpenCL kernel on the list
-        clEnqueueNDRangeKernel(cque,
-                               kernel,
-                               1,
-                               ptr::null(),
-                               ptr::to_unsafe_ptr(&global_item_size),
-                               ptr::to_unsafe_ptr(&local_item_size),
-                               0,
-                               ptr::null(),
-                               ptr::null());
+				libc::free(buf);
 
-        // Now let's read back the new list from the device
-        let buf = libc::malloc((sz * sys::size_of::<int>()) as libc::size_t);
+				println(fmt!("	%?", vec_a));
+				println(fmt!("+	%?", vec_b));
+				println(fmt!("=	%?", vec_c));
 
-        clEnqueueReadBuffer(cque,
-                            C,
-                            CL_TRUE,
-                            0,
-                            (sz * sys::size_of::<int>()) as libc::size_t,
-                            buf,
-                            0,
-                            ptr::null(),
-                            ptr::null());
-
-        let vec_c = vec::from_buf(buf as *int, sz);
-
-        libc::free(buf);
-
-        io::println(fmt!("   %?", vec_a));
-        io::println(fmt!("+  %?", vec_b));
-        io::println(fmt!("=  %?", vec_c));
-
-        // Cleanup
-        clReleaseKernel(kernel);
-        clReleaseProgram(prog);
-        clReleaseMemObject(C);
-        clReleaseMemObject(B);
-        clReleaseMemObject(A);
-        clReleaseCommandQueue(cque);
-        clReleaseContext(ctx);
-      }
-    }
-  }
+				// Cleanup
+//				clReleaseKernel(kernel);
+//				clReleaseProgram(prog);
+			}
+		}
+	}
 }
