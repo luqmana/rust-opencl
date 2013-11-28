36364ba3588ea92fbfe9d3faf02d385b684092b0
Colin Sherratt <colin.sherratt@gmail.com>
2013-11-28 16:40:36 -0500

    added create_compute_context_prefer

--- src/OpenCL/util.rs
+++ src/OpenCL/util.rs
@@ -17,3 +17,40 @@ pub fn create_compute_context() -> Result<(Device, Context, CommandQueue), ~str>
 
     Ok((devices[0], context, queue))
 }
+
+
+pub enum PreferedType {
+    ANY,
+
+    CPU_PREFERED,
+    GPU_PREFERED,
+
+    CPU_ONLY,
+    GPU_ONLY,
+
+}
+
+pub fn create_compute_context_prefer(cltype: PreferedType) -> Result<(Device, Context, CommandQueue), ~str>
+{
+    let platforms = get_platforms();
+    for platform in platforms.iter() {
+        let types = match cltype {
+            ANY => &[CPU, GPU],
+            CPU_PREFERED | CPU_ONLY => &[CPU],
+            GPU_PREFERED | GPU_ONLY => &[GPU]
+        };
+
+        let devices = platform.get_devices_by_types(types);
+        if devices.len() != 0 {
+            let context = devices[0].create_context();
+            let queue = context.create_command_queue(&devices[0]);
+            return Ok((devices[0], context, queue));
+        } 
+    }
+
+
+    match cltype {
+        ANY | CPU_PREFERED | GPU_PREFERED => create_compute_context(),
+        _ => Err(~"Could not find valid implementation")    
+    }
+}
