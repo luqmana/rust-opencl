2844a270612cced0a6c1794470c4a975ff93c4df
Ben Foppa <benjamin.foppa@gmail.com>
2015-01-17 14:44:06 -0500

    Fix String impl for CLStatus.

--- src/cl.rs
+++ src/cl.rs
@@ -134,7 +134,7 @@ pub enum CLStatus {
 
 impl fmt::String for CLStatus {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self)
+        write!(f, "{:?}", self)
     }
 }
 
