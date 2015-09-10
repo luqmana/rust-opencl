f441c5b80e79d6af7ad42768c27e1d7e81b22b64
Zatvobor <a.zatvornitskiy@gmail.com>
2015-09-10 18:32:44 +0300

    Got some more fields for platform example

--- examples/platform/main.rs
+++ examples/platform/main.rs
@@ -5,12 +5,16 @@ use opencl::hl;
 fn main() {
     for platform in hl::get_platforms().iter() {
         println!("Platform: {}", platform.name());
+        println!("Platform Version: {}", platform.version());
         println!("Vendor:   {}", platform.vendor());
         println!("Profile:  {}", platform.profile());
         println!("Available extensions: {}", platform.extensions());
         println!("Available devices:");
         for device in platform.get_devices().iter() {
-            println!("   {}", device.name());
+            println!("   Name: {}", device.name());
+            println!("   Type: {}", device.device_type());
+            println!("   Profile: {}", device.profile());
+            println!("   Compute Units: {}", device.compute_units());
         }
     }
 }
