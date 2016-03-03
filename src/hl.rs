db07a7750a027f341ec31ea8d200e84a4832dd42
Theodore DeRego <tderego94@gmail.com>
2016-03-02 16:00:12 -1000

    Fixed KernelArg Vec and slice sizes

--- src/hl.rs
+++ src/hl.rs
@@ -707,14 +707,14 @@ scalar_kernel_arg!([f64; 2]);
 
 impl<T: Copy> KernelArg for Vec<T> {
     fn get_value(&self) -> (libc::size_t, *const libc::c_void) {
-        (4 * mem::size_of::<T>() as libc::size_t,
+        ((self.len() * mem::size_of::<T>()) as libc::size_t,
           (self.as_ptr()) as *const libc::c_void)
     }
 }
 
 impl<'a, T: Copy> KernelArg for &'a [T] {
     fn get_value(&self) -> (libc::size_t, *const libc::c_void) {
-        (4 * mem::size_of::<T>() as libc::size_t,
+        ((self.len() * mem::size_of::<T>()) as libc::size_t,
           (self.as_ptr()) as *const libc::c_void)
     }
 }
